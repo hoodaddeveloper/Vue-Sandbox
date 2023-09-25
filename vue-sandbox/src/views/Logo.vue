76b6efebfd5a7df0850a8c1d6e82445fe002dce0
hoodaddeveloper <mohammadkhaffas@gmail.com>
2023-09-25 16:05:41 +0200

    editing a component

--- vue-sandbox/src/views/Logo.vue
+++ vue-sandbox/src/views/Logo.vue
@@ -4,6 +4,15 @@
 
 <script>
 // javaScript codes
+export default {
+  name: "Logo",
+  data() {
+    return {
+      name: "My first vue sandbox project",
+      creator: "HoodadDeveloper"
+    }
+  }
+}
 </script>
 
 <style scoped> /* Only for this cpmponent */
