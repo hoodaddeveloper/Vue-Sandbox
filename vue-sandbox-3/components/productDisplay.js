64ccd820a10bfea59a8a50bb0588d65b89020740
hoodaddeveloper <mohammadkhaffas@gmail.com>
2023-10-03 18:17:12 +0200

    updated cart

--- vue-sandbox-3/components/productDisplay.js
+++ vue-sandbox-3/components/productDisplay.js
@@ -1,33 +1,41 @@
 app.component("product-display", {
+  props: {
+    premium: {
+      type: Boolean,
+      required: true
+    }
+  },
   template:
-  /*html*/
- `<div class="product-display">
-    <div class="product-container">
+    /*html*/
+    `<div class="product-display">
+      <div class="product-container">
 
-    <div class="product-image">
-      <img v-bind:src="image">
-    </div>
+        <div class="product-image">
+          <img v-bind:src="image">
+        </div>
 
-    <div class="product-info">
-      <h1>{{ title }}</h1>
-      <p v-if="inventory >= 10">In Stock</p>
-      <p v-else-if="inventory <= 10 && inventory > 0">Almost sold out!</p>
-      <p v-else>Out of Stock</p>
-      <ul>
-        <li v-for="detail in details">{{ detail }}</li>
-      </ul>
-      <div v-for="(variant, index) in variants" :key="variant.id" @mouseover="updateVariant(index)"
-        class="color-circle" :style="{ backgroundColor: variant.color }">
+        <div class="product-info">
+          <h1>{{ title }}</h1>
+          <p v-if="inventory >= 10">In Stock</p>
+          <p v-else-if="inventory <= 10 && inventory > 0">Almost sold out!</p>
+          <p v-else>Out of Stock</p>
+
+          <p>Shipping: {{ shipping }}</p>
+          <ul>
+            <li v-for="detail in details">{{ detail }}</li>
+          </ul>
+          <div v-for="(variant, index) in variants" :key="variant.id" @mouseover="updateVariant(index)"
+            class="color-circle" :style="{ backgroundColor: variant.color }">
+          </div>
+
+          <button class="button" :disabled="!inventory" :class="{disabledButton: !inventory}"
+            v-on:click="addToCart">Add to Cart</button>
+          <button class="button" :disabled="!inventory" :class="{disabledButton: !inventory}"
+            v-on:click="removeFromCart">Remove</button>
+        </div>
+
       </div>
-
-      <button class="button" :disabled="!inventory" :class="{disabledButton: !inventory}" v-on:click="addToCart">Add
-        to Cart</button>
-      <button class="button" :disabled="!inventory" :class="{disabledButton: !inventory}"
-        v-on:click="removeFromCart">Remove</button>
-    </div>
-
-   </div>
-  </div>`,
+    </div>`,
   data() {
     return {
       product: 'Socks',
@@ -42,10 +50,10 @@ app.component("product-display", {
   },
   methods: {
     addToCart() {
-      this.cart += 1
+      this.$emit("add-to-cart")
     },
     removeFromCart() {
-      this.cart -= 1
+      this.$emit("remove-from-cart")
     },
     updateVariant(index) {
       this.selectedVariant = index
@@ -60,6 +68,12 @@ app.component("product-display", {
     },
     inventory() {
       return this.variants[this.selectedVariant].quantity
+    },
+    shipping() {
+      if (this.premium) {
+        return "Free"
+      }
+      return "2.99$"
     }
   }
 })
