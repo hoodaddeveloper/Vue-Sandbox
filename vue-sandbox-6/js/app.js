a50c3992705f0cacc9b0826eb9e03fa43347d846
hoodaddeveloper <mohammadkhaffas@gmail.com>
2023-10-15 16:09:07 +0200

    created the app

--- vue-sandbox-6/js/app.js
+++ vue-sandbox-6/js/app.js
@@ -1,13 +1,3 @@
 const app = new Vue({
-  el: "#app",
-  data: {
-    username: 'HoodadDeveloper',
-    newTweet: '',
-    tweets: [
-      'Started learning to code today. Wish me luck!',
-      'Okay, I learned HTML, CSS, and JavaScript. But, how do I combine them together?? Send help.',
-      'Today I start learning Vue. I got this.'
-    ],
-    bio: 'Excited future front-end engineer.'
-  }
+  el: "#app"
 });
