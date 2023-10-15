13f00bdcc8a243d1b599d35b6711034a46bae24b
hoodaddeveloper <mohammadkhaffas@gmail.com>
2023-10-15 16:34:46 +0200

    finished

--- vue-sandbox-6/js/app.js
+++ vue-sandbox-6/js/app.js
@@ -1,41 +1,70 @@
 const app = new Vue({
-  el: "#app",
+  el: '#app',
   data: {
-    firstName: "First",
-    lastName: "Last",
-    email: "",
+    firstName: '',
+    lastName: '',
+    email: '',
     ticketQuantity: 1,
-    ticketType: "general",
+    ticketType: 'general',
     referrals: [],
-    specialRequests: "",
-    purchaseAgreementSigned: false,
+    specialRequests: '',
+    purchaseAgreementSigned: false
   },
   computed: {
-    fullName: function () {
-      if (this.firstName && this.lastName) {
-        return this.firstName + " " + this.lastName;
-      } else {
-        return this.firstName || this.lastName;
+    fullName: {
+      get: function () {
+        if (this.firstName && this.lastName) {
+          return this.firstName + ' ' + this.lastName;
+        } else {
+          return this.firstName || this.lastName;
+        }
+      },
+      set: function (newFullName) {
+        const names = newFullName.split(' ');
+
+        if (names.length === 2) {
+          this.firstName = names[0];
+          this.lastName = names[1];
+        }
+
+        if (names.length <= 1) {
+          this.firstName = names[0] || '';
+          this.lastName = '';
+        }
       }
     },
     ticketDescription: function () {
-      let readableTicketType = "General Admission";
-      if (this.ticketType === "vip") {
-        readableTicketType = "VIP";
+      let readableTicketType = 'General Admission';
+      if (this.ticketType === 'vip') {
+        readableTicketType = 'VIP';
       }
 
-      let ticketPluralization = "tickets";
+      let ticketPluralization = 'tickets';
       if (this.ticketQuantity === 1) {
-        ticketPluralization = "ticket";
+        ticketPluralization = 'ticket';
       }
 
-      return (
-        this.ticketQuantity +
-        " " +
-        readableTicketType +
-        " " +
-        ticketPluralization
-      );
-    },
+      return this.ticketQuantity + ' ' + readableTicketType + ' ' + ticketPluralization;
+    }
   },
+  watch: {
+    specialRequests: function (newRequests, oldRequests) {
+      if (newRequests.toLowerCase().includes('meet and greet') ||
+        newRequests.toLowerCase().includes('meet-and-greet')) {
+        this.ticketType = 'vip';
+      }
+    }
+  },
+  methods: {
+    resetFields: function () {
+      this.firstName = '';
+      this.lastName = '';
+      this.email = '';
+      this.ticketQuantity = 1;
+      this.ticketType = 'general';
+      this.referrals = [];
+      this.specialRequests = '';
+      this.purchaseAgreementSigned = false;
+    }
+  }
 });
